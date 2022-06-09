3cc9ae7998108219cc51b89312aab1b554a5f0fb
Nathan Travers <NNathann@pm.me>
2022-06-09 22:38:28 +0100

    Create Screenshots/ folder\nAdd gifs for three modes\nMode 2 - just kinda mess it up, I know how I want to rebuild it completely and remove the nonsense legacy comments\nNaming scheme - Main 1,2,3 are now referred to as Mode 0,1,2 respectively

---
+++ README.txt
@@ -0,0 +1,12 @@
+# Wave automata
+Me trying to create some cool behaviour and physics in a cellular automaton.
+Currently, I haven't put enough brainpower into figuring out good rulesets or even how to define them.
+
+For now, here are some cool patterns from trying to make pixels into factions which fight/eat each other:
+## Example gifs
+### Mode 0
+![Mode0.gif](screenshots/wave-automata_mode0_100speed.gif)
+### Mode 1
+![Mode1.gif](screenshots/wave-automata_mode1_150speed.gif)
+### Mode 2
+![Mode2.gif](screenshots/wave-automata_mode2_150speed.gif)
--- wave automata.py
+++ wave automata.py
@@ -102,11 +102,6 @@ if cell < cell_neighbour_average:
 else:
     cell -= .1"""
 
-#so the name of this app was after quantum fields idea with a wave being a plane, trying to make that in an cellular automata
-#now its kinda a wave function thing, which suits the name, but I didn't think that until having wavefunction collapse come to mind
-#from some youtube video, googled this https://robertheaton.com/2018/12/17/wavefunction-collapse-algorithm/
-#not really a wavefunction collapse but ideas
-
 
 
 
@@ -218,19 +213,28 @@ def main1():
 
 
 
-def update_grid2(grid):
+def update_grid2(grid, x):
     grid_dimensions = grid.shape[:2]
-    grid_shuffled = list(zip([idx for idx in range(grid_dimensions[0])], grid))
-    shuffle(grid_shuffled)
-    for row_number, row in grid_shuffled:
-        row_shuffled = list(zip([idx for idx in range(grid_dimensions[1])], row))
-        shuffle(row_shuffled)
+    #grid_shuffled = list(zip(, grid))
+    #shuffle(grid_shuffled)
+    row_numbers = [idx for idx in range(grid_dimensions[0])]
+    column_numbers = [idx1 for idx1 in range(grid_dimensions[1])]
+    shuffle(row_numbers)
+    
+    for row_number in row_numbers:
+        shuffle(column_numbers)
+        
+        #row_shuffled = list(zip([idx for idx in range(grid_dimensions[1])], row))
+        #shuffle(row_shuffled)
         """row_unshuffled = enumerate(row)
         if randint(0,4) != 0:
             row_chosen = row_shuffled
         else:
             row_chosen = row_unshuffled"""
-        for column_number, cell in row_shuffled:
+            
+        for column_number in column_numbers:
+            #if randint(0,25000) == 0:
+            #    grid[row_number, column_number:randint(0,99-column_number)] = (grid[row_number, column_number][0],0)
             cell_above = 0
             cell_below = 0
             cell_left = 0
@@ -265,7 +269,7 @@ def update_grid2(grid):
             shuffle(neighbours)
 
             friends = 0
-            cell_colour, cell_age = cell
+            cell_colour, cell_age = grid[row_number, column_number] #col and row number were backwards, cool y=x reflect wave pattern tho
             enemy_pos = (row_number, column_number)
             for neighbour_pos in neighbours:
                 #neighbour = choice(neighbours)
@@ -279,24 +283,61 @@ def update_grid2(grid):
             enemy_colour, enemy_age = grid[enemy_pos]
             if enemy_pos != (row_number, column_number):
                 if friends >= 1:# and randint(0,4) != 0:
-                    #if enemy_age > cell_age:
-                    #    enemy_colour = cell_colour
-                    #    enemy_age = 0
-                    if enemy_age > cell_age or randint(0,4)==0: #enemy_age < cell_age (without or) creates a cool blood-cell looking pattern
-                        enemy_colour = cell_colour
-                        enemy_age = 0
-                elif randint(0,5000)==0:
+                    if enemy_age > cell_age: #enemy_age < cell_age creates a cool blood-cell looking pattern
+                        if enemy_age - cell_age > 10:
+                            enemy_colour = cell_colour
+                            enemy_age = cell_age# + 1 #- 2#enemy_age - enemy_age/4
+                        else:
+                            enemy_colour = cell_colour # i can think of better rulesets now i just need to rewrite this entire block make it variable
+                            enemy_age = cell_age - 1# + 1 #- 2#enemy_age - enemy_age/4
+                    #elif enemy_age == cell_age:
+                     #   enemy_age -= 
+                        #enemy_age += 10
+                        #cell_age -= 100
+                        pass
+                        """if randint(0,2)!=0:
+                            enemy_colour = cell_colour
+                            #cell_age -= 10
+                            enemy_age = - enemy_age# - 10#cell_age
+                        else:
+                            pass
+                            #enemy_age -= 30
+                            #cell_colour = enemy_colour
+                            #cell_age = enemy_age - 10"""
+
+                    #elif randint(0,200000)==0:
+                    #    pass
+                        #cell_age += 2
+                #elif friends < 2 and cell_age > 0:
+                #    cell_colour = enemy_colour
+                #    cell_age = 0
+                elif randint(0,10000)==0:
                     cell_colour = enemy_colour
-                    cell_age = 0
+                    #cell_age = enemy_age - 10
+            else: #no enemies
+                """young_neighbour = False
+                for neighbour_pos in neighbours:
+                    neighbour_colour, neighbour_age = grid[neighbour_pos]
+                    if abs(cell_age - neighbour_age) > 20: #already a young warrior around
+                        young_neighbour = True
+                        break
+                if not young_neighbour:
+                    cell_age -= 21"""
+                #cell_age += 1
+
             if enemy_age != 200:
                 enemy_age += 1
                     
             if cell_age != 200:
                 cell_age += 1
-            cell = (cell_colour, cell_age)
-            enemy = (enemy_colour, enemy_age)
-            grid[row_number, column_number] = cell
-            grid[enemy_pos] = enemy
+
+            if cell_age<0:
+                cell_age=0
+            if enemy_age<0:
+                enemy_age=0
+                
+            grid[row_number, column_number] = (cell_colour, cell_age)
+            grid[enemy_pos] = (enemy_colour, enemy_age)
             
 def generate_canvas2(grid, grid_dimensions):
     size = 1
@@ -304,29 +345,35 @@ def generate_canvas2(grid, grid_dimensions):
     for row_no, row in enumerate(grid):
         for pixel_no, pixel in enumerate(row):
             colour, age = pixel
-            canvas[(size*row_no):(size + size*row_no), (size*pixel_no):(size + size*pixel_no)] = [colour*255, 0, 0, 255-age]
+            canvas[(size*row_no):(size + size*row_no), (size*pixel_no):(size + size*pixel_no)] = [0, 0, colour*255, 255-age]
     return canvas
 
 def main2():
-    grid_dimensions=(100,100)
+    grid_dimensions=(80,80)
     #grid = rand(*grid_dimensions)
-    grid = asarray([(rand(), 0) for _ in range(100*100)])
-    grid = grid.reshape(100,100,2)
+    grid = asarray([(rand(), 0) for _ in range(grid_dimensions[0]*grid_dimensions[1])])
+    grid = grid.reshape(grid_dimensions[0],grid_dimensions[1],2)
     #grid = full((*grid_dimensions, 2), )
         
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     im = ax.imshow(generate_canvas2(grid, grid_dimensions), animated=True)
+    x=0
     def update_image(image):
-        update_grid2(grid)#randomise=1)
+        update_grid2(grid, x)#randomise=1)
         canvas = generate_canvas2(grid, grid_dimensions)
         im.set_array(canvas)
 
     ani = animation.FuncAnimation(fig, update_image, interval=4)
-    plt.show()#block = False
+    plt.show()#block = False)
 
 if __name__=="__main__":
+    from time import sleep
     if bool(input("Nothing for mode 0, anything for mode 1\nEnter input: ")):
-        main1()
+        sleep(5)
+        main()
     else:
-        main2()
+        sleep(5)
+        main1()
+
+#this code is ungodly amounts of messy because its just freeflow building stuff, still I'll make it cleaner next time...
